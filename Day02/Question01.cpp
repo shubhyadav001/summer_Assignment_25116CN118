acab34a73c8c5776193c8d802fc1b4bd4a58691c
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-12 22:48:27 +0530

    commit new

--- Day02/Question01.cpp
+++ Day02/Question01.cpp
@@ -1,2 +1,26 @@
 //Write a program to Find sum of digits of a number.
 
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(long long n) {
+    // Handle negative numbers
+    if (n < 0) n = -n;
+    
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    long long num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    cout << "Sum of digits of " << num << " = " << sumOfDigits(num) << endl;
+    
+    return 0;
+}
