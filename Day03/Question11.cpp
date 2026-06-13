e35a7d9bd23098c275e6fec035b47163868a4a25
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-13 22:50:13 +0530

    commit all

---
+++ Day03/Question11.cpp
@@ -0,0 +1,20 @@
+//Write a program to Find GCD of two numbers. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    cout << "GCD = " << a;
+
+    return 0;
+}
