62b9032415ab81afbe7f9325bd05a6f9251a1daf
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-16 23:38:56 +0530

    commit all

---
+++ Day06/Question24.cpp
@@ -0,0 +1,22 @@
+//Write a program to Find x^n without pow(). 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, n;
+    long long result = 1;
+
+    cout << "Enter base (x): ";
+    cin >> x;
+
+    cout << "Enter exponent (n): ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        result *= x;
+    }
+
+    cout << x << "^" << n << " = " << result;
+
+    return 0;
+}
