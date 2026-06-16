cec1811755706c79ee5bc1260862e8eebb5c83f9
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-16 23:36:29 +0530

    commit all

---
+++ Day06/Question23.cpp
@@ -0,0 +1,21 @@
+//Write a program to Count set bits in a number. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num > 0) {
+        if (num % 2 == 1)
+            count++;
+
+        num /= 2;
+    }
+
+    cout << "Number of set bits = " << count;
+
+    return 0;
+}
