9041275e26dff6be97c7a9efd834f3c84b83ac7e
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-18 22:20:11 +0530

    commit all

---
+++ Day08/Question30.cpp
@@ -0,0 +1,25 @@
+//Write a program to Print number triangle.
+//1
+//12
+//123
+//1234
+//12345
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
