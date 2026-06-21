f480367f9bdee2a074f536669b3fe2ae5207f846
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-21 18:48:52 +0530

    commit all

---
+++ Day11/Question41.cpp
@@ -0,0 +1,21 @@
+//Write a program to Write function to find sum of two numbers.
+
+
+
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Sum = " << sum(num1, num2);
+
+    return 0;
+}
