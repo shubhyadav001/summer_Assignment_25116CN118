d4d746412f05afef66e882d01385e2da80b8da83
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-21 23:15:57 +0530

    commit all

---
+++ Day11/Question44.cpp
@@ -0,0 +1,24 @@
+//Write a program to Write function to find factorial.
+#include <iostream>
+using namespace std;
+
+long long factorial(int n) {
+    long long fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial = " << factorial(num);
+
+    return 0;
+}
