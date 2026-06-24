b439eecdb421392b5649a6c91b17b2c0f953dbd5
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-24 21:57:40 +0530

    commit all

---
+++ Day14/Question53.cpp
@@ -0,0 +1,36 @@
+//Write a program to Linear search. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int pos = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            pos = i;
+            break;
+        }
+    }
+
+    if (pos != -1)
+        cout << "Element found at position " << pos + 1;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
