55fc571917ef500d7692dfcab38818175ba07b6b
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-25 21:14:37 +0530

    commit all

---
+++ Day15/Question57.cpp
@@ -0,0 +1,25 @@
+//Write a program to Reverse array. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reversed Array: ";
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
