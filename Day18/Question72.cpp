4134b3eac1a97db1cf779ec2cfb1f55e0d476ba1
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-06-28 22:14:18 +0530

    commit all

---
+++ Day18/Question72.cpp
@@ -0,0 +1,35 @@
+//Write a program to Sort array in descending order.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sort in descending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Array in Descending Order: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
