9d038cbd55c0a4c499c87711c8abc4eda6987af8
shubhyadav001 <shubhamyadav65309@gmail.com>
2026-07-03 23:44:37 +0530

    commit all

---
+++ Day23/Question89.cpp
@@ -0,0 +1,28 @@
+//Write a program to Find first non-repeating character.
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    // Count frequency of each character
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(int)str[i]]++;
+    }
+
+    // Find the first non-repeating character
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (freq[(int)str[i]] == 1) {
+            cout << "First non-repeating character: " << str[i] << endl;
+            return 0;
+        }
+    }
+
+    cout << "No non-repeating character found." << endl;
+
+    return 0;
+}
